d82019f517ff9a88b7b118f9b564962856161796
Norio Nomura <norio.nomura@gmail.com>
2019-09-25 11:29:37 +0900

    Drop Swift 4.1

--- Sources/Base32/shim.swift
+++ Sources/Base32/shim.swift
@@ -1,26 +1,6 @@
 import Foundation
 
-#if !swift(>=4.2)
-extension Collection {
-    public func firstIndex(
-        where predicate: (Element) throws -> Bool
-    ) rethrows -> Index? {
-        return try index(where: predicate)
-    }
-}
-#endif
-
-#if swift(>=4.2)
 #if !compiler(>=5)
-extension Data {
-    func withUnsafeBytes<Result>(_ apply: (UnsafeRawBufferPointer) throws -> Result) rethrows -> Result {
-        return try withUnsafeBytes {
-            try apply(UnsafeRawBufferPointer(start: $0, count: count))
-        }
-    }
-}
-#endif
-#else
 extension Data {
     func withUnsafeBytes<Result>(_ apply: (UnsafeRawBufferPointer) throws -> Result) rethrows -> Result {
         return try withUnsafeBytes {
