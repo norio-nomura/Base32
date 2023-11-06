d7693dc19252ae0d505ea70245a699dc216b8069
Tomasz Pietrek <melgaer@gmail.com>
2023-11-06 19:13:09 +0100

    Add tests for base32 without padding
    Signed-off-by: Tomasz Pietrek <melgaer@gmail.com>

--- Tests/Base32Tests/Base32Tests.swift
+++ Tests/Base32Tests/Base32Tests.swift
@@ -30,14 +30,14 @@ import XCTest
 
 class Base32Tests: XCTestCase {
     
-    let vectors: [(String,String,String)] = [
-        ("", "", ""),
-        ("f", "MY======", "CO======"),
-        ("fo", "MZXQ====", "CPNG===="),
-        ("foo", "MZXW6===", "CPNMU==="),
-        ("foob", "MZXW6YQ=", "CPNMUOG="),
-        ("fooba", "MZXW6YTB", "CPNMUOJ1"),
-        ("foobar", "MZXW6YTBOI======", "CPNMUOJ1E8======"),
+    let vectors: [(String,String,String, String, String)] = [
+        ("", "", "", "", ""),
+        ("f", "MY======", "MY", "CO======", "CO"),
+        ("fo", "MZXQ====", "MZXQ", "CPNG====", "CPNG"),
+        ("foo", "MZXW6===","MZXW6", "CPNMU===", "CPNMU"),
+        ("foob", "MZXW6YQ=","MZXW6YQ", "CPNMUOG=", "CPNMUOG"),
+        ("fooba", "MZXW6YTB","MZXW6YTB", "CPNMUOJ1", "CPNMUOJ1"),
+        ("foobar", "MZXW6YTBOI======","MZXW6YTBOI", "CPNMUOJ1E8======", "CPNMUOJ1E8"),
     ]
     
     override func setUp() {
@@ -53,48 +53,58 @@ class Base32Tests: XCTestCase {
     // MARK: https://tools.ietf.org/html/rfc4648
     
     func test_RFC4648_base32Encode() {
-        let convertedVectors = self.vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2)}
+        let convertedVectors = self.vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2, $3, $4)}
         self.measure{
             for _ in 0...100 {
-                for (test, expect, _) in convertedVectors {
+                for (test, expect, expectNotPadded, _, _) in convertedVectors {
                     let result = base32Encode(test)
                     XCTAssertEqual(result, expect, "base32Encode for \(test)")
+                    
+                    let not_padded_result = base32Encode(test, padding: false)
+                    XCTAssertEqual(not_padded_result, expectNotPadded, "base32Encode without padding for \(test)")
                 }
             }
         }
     }
     
     func test_RFC4648_base32Decode() {
-        let convertedVectors = self.vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2)}
+        let convertedVectors = self.vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2, $3, $4)}
         self.measure{
             for _ in 0...100 {
-                for (expect, test, _) in convertedVectors {
+                for (expect, test, not_padded_test, _, _) in convertedVectors {
                     let result = base32DecodeToData(test)
                     XCTAssertEqual(result!, expect, "base32Decode for \(test)")
+                    
+                    let not_padded_result = base32DecodeToData(not_padded_test)
+                    XCTAssertEqual(not_padded_result!, expect, "not padded base32Decode for \(not_padded_test)")
                 }
             }
         }
     }
     
     func test_RFC4648_base32HexEncode() {
-        let convertedVectors = self.vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2)}
+        let convertedVectors = self.vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2, $3, $4)}
         self.measure{
             for _ in 0...100 {
-                for (test, _, expectHex) in convertedVectors {
+                for (test, _, _, expectHex, notPaddedexpectHex) in convertedVectors {
                     let resultHex = base32HexEncode(test)
                     XCTAssertEqual(resultHex, expectHex, "base32HexEncode for \(test)")
+                    let notPaddedResult = base32HexEncode(test, padding: false)
+                    XCTAssertEqual(notPaddedResult, notPaddedexpectHex, "not padded base32HexEncode for \(test)")
                 }
             }
         }
     }
     
     func test_RFC4648_base32HexDecode() {
-        let convertedVectors = self.vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2)}
+        let convertedVectors = self.vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2, $3, $4)}
         self.measure{
             for _ in 0...100 {
-                for (expect, _, testHex) in convertedVectors {
+                for (expect, _, _, testHex, notPaddedExpectHex) in convertedVectors {
                     let resultHex = base32HexDecodeToData(testHex)
                     XCTAssertEqual(resultHex!, expect, "base32HexDecode for \(testHex)")
+                    let notPaddedResultHex = base32HexDecodeToData(notPaddedExpectHex)
+                    XCTAssertEqual(notPaddedResultHex!, expect, "not padded base32HexDecode for \(testHex)")
                 }
             }
         }
@@ -105,15 +115,23 @@ class Base32Tests: XCTestCase {
     func test_base32ExtensionString() {
         self.measure{
             for _ in 0...100 {
-                for (test, expect, expectHex) in self.vectors {
+                for (test, expect, notPaddedExpect, expectHex, notPaddedHexExpect) in self.vectors {
                     let result = test.base32EncodedString
+                    let notPaddedResult = test.base32EncodedStringNoPadding
                     let resultHex = test.base32HexEncodedString
+                    let NotPaddedResultHex = test.base32HexEncodedStringNoPadding
                     XCTAssertEqual(result, expect, "\(test).base32EncodedString")
+                    XCTAssertEqual(notPaddedResult, notPaddedExpect, "\(test).base32EncodedStringNoPadding")
                     XCTAssertEqual(resultHex, expectHex, "\(test).base32HexEncodedString")
+                    XCTAssertEqual(NotPaddedResultHex, notPaddedHexExpect, "\(test).base32HexEncodedStringNoPadding")
                     let decoded = result.base32DecodedString()
+                    let notPaddedDecoded = notPaddedResult.base32DecodedString()
                     let decodedHex = resultHex.base32HexDecodedString()
+                    let notPaddedDecodedHex = NotPaddedResultHex.base32HexDecodedString()
                     XCTAssertEqual(decoded!, test, "\(result).base32DecodedString()")
+                    XCTAssertEqual(notPaddedDecoded!, test, "not padded \(result).base32DecodedString()")
                     XCTAssertEqual(decodedHex!, test, "\(resultHex).base32HexDecodedString()")
+                    XCTAssertEqual(notPaddedDecodedHex!, test, "not padded \(resultHex).base32HexDecodedString()")
                 }
             }
         }
@@ -124,12 +142,14 @@ class Base32Tests: XCTestCase {
             (
                 $0.dataUsingUTF8StringEncoding,
                 $1.dataUsingUTF8StringEncoding,
-                $2.dataUsingUTF8StringEncoding
+                $2.dataUsingUTF8StringEncoding,
+                $3.dataUsingUTF8StringEncoding,
+                $4.dataUsingUTF8StringEncoding
             )
         }
         self.measure{
             for _ in 0...100 {
-                for (test, expect, expectHex) in dataVectors {
+                for (test, expect, _, expectHex, _) in dataVectors {
                     let result = test.base32EncodedData
                     let resultHex = test.base32HexEncodedData
                     XCTAssertEqual(result, expect, "\(test).base32EncodedData")
@@ -144,10 +164,10 @@ class Base32Tests: XCTestCase {
     }
     
     func test_base32ExtensionDataAndString() {
-        let dataAndStringVectors = vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2)}
+        let dataAndStringVectors = vectors.map {($0.dataUsingUTF8StringEncoding, $1, $2, $3, $4)}
         self.measure{
             for _ in 0...100 {
-                for (test, expect, expectHex) in dataAndStringVectors {
+                for (test, expect, _,  expectHex, _) in dataAndStringVectors {
                     let result = test.base32EncodedString
                     let resultHex = test.base32HexEncodedString
                     XCTAssertEqual(result, expect, "\(test).base32EncodedString")
@@ -169,10 +189,12 @@ class Base32Tests: XCTestCase {
             (
                 $0.dataUsingUTF8StringEncoding,
                 $1.replacingOccurrences(of: "=", with:""),
-                $2.replacingOccurrences(of: "=", with:"")
+                $2.replacingOccurrences(of: "=", with:""),
+                $3.replacingOccurrences(of: "=", with:""),
+                $4.replacingOccurrences(of: "=", with:"")
             )
         }
-        for (expect, test, testHex) in strippedVectors {
+        for (expect, test, _, testHex, _) in strippedVectors {
             let result = base32DecodeToData(test)
             let resultHex = base32HexDecodeToData(testHex)
             XCTAssertEqual(result!, expect, "base32Decode for \(test)")
